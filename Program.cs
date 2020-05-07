5da83736213f3118c42e6fa2a013f8f60c756947
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-07 11:04:38 -0700

    framework & while loop to play

--- Program.cs
+++ Program.cs
@@ -9,8 +9,39 @@ namespace Garden
   {
     public static void Main()
     {
-      Console.WriteLine("Let's go on a herb adventure!");
+      Console.WriteLine("Let's go on a herb adventure!  What would you like to name your plant?");
+      string myName = Console.ReadLine();
 
+      Herb myHerb = new Herb(myName, 1, 0, 1);
+
+      Console.WriteLine("Your plant " + myName + "is ready for you to take care of it!");
+      
+      while (myHerb.health > 0) 
+      {
+        Console.WriteLine("What would you like to do?  Enter 'water', 'sun', or 'feed', 'exit'.");
+        string action = Console.ReadLine();
+
+        if (action == "water")
+        {
+          myHerb.Height = myHerb.Height + 1;
+        }
+        else if (action == "sun")
+        {
+          
+        }
+        else if (action == "feed")
+        {
+          
+        }
+        else if (action == "exit")
+        {
+
+        }
+        else
+        {
+          Console.WriteLine("Sorry I didn't understand your input");
+        }
+      }
     }
   }
 }
